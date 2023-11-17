24d3cf54f041ad3fdb9e43020ad3099ef7ad0977
edvardas1996 <edvardasmikulenas@gmail.com>
2023-11-17 11:32:21 +0200

    working option

--- src/test/java/VynotekaTest.java
+++ src/test/java/VynotekaTest.java
@@ -18,7 +18,7 @@ public class VynotekaTest {
     public void beforeClass() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        wait= new WebDriverWait(driver,Duration.ofSeconds(400));
+        wait= new WebDriverWait(driver,Duration.ofSeconds(30));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         acceptCookiesAndAge();
     }
@@ -36,14 +36,11 @@ public class VynotekaTest {
 
     @Test
     public void adPositiveTest() {
-      //  driver.findElement(By.id("omnisend-forms-container")).findElement(By.tagName("button"));
-        driver.get("https://vynoteka.lt/");//*[@id="omnisend-forms-container"]/div/div[2]/div[2]/div[2]/button
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("omnisend-forms-container")));
-        driver.findElement(By.id("omnisend-forms-container")).findElement(By.tagName("button")).click();
-      //  wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"omnisend-form-63ff1f31b40d6530aba59a6d-close-action\"]/div"))).click();
-//        driver.findElement(By.xpath("//*[@id=\"omnisend-form-63ff1f31b40d6530aba59a6d-form-close-icon\"]/path")).click();
+        driver.get("https://vynoteka.lt/");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"omnisend-form-63ff1f31b40d6530aba59a6d-close-action\"]/div"))).click();
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"omnisend-form-63ff1f31b40d6530aba59a6d-close-action\"]/div"))).click();
         driver.findElement(By.xpath("//*[@id=\"app__header\"]/div[2]/div/div/div[3]/div/div/div/form/div[1]/div/input")).sendKeys("Jagermeister 1 L");
-        driver.findElement(By.xpath("//*[@id=\"app__header\"]/div[2]/div/div/div[3]/div/div/div/form/div[1]/button")).click();
+        driver.findElement(By.xpath("//*[@id=\"app__header\"]/div[2]/div/div/div[3]/div/div/div/form/div[2]/div/div[2]/div/div/div/div/div[1]/div/div/a")).click();
 
     }
 
